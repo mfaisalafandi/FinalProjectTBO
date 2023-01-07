0265dfb626a3796877d22c05c03df909cbbad5ed
Faisal Afandi <58682316+mfaisalafandi@users.noreply.github.com>
2023-01-07 08:52:41 +0800

    Update CFG.py

--- CFG.py
+++ CFG.py
@@ -1,3 +1,4 @@
+# Membaca file dan memasukkan datanya pada list data
 def open_file(filepath):
     data = []
     with open(filepath, 'r') as file:
@@ -5,13 +6,15 @@ def open_file(filepath):
         for rule in raw:
             data.append(rule.strip('\n'))
     return data
-
+# Memisahkan data pada file cnf.txt antara key dan body
 def raw_to_cfg(raw):
     cfg = []
     for line in raw:
+        # Memisahkan head & body dari rule cnf
         cfg.append(line.split(' -> '))
     for rule in cfg:
-        rule[1] = set(rule[1].split(' | '))
+        # Memisahkan setiap rule dari body cnf
+        rule[1] = set(rule[1].split('|'))
     for rule in cfg:
         new_body = set()
         for element in rule[1]:
@@ -19,3 +22,4 @@ def raw_to_cfg(raw):
             new_body.add(element_to_tuple)
         rule[1] = new_body
     return cfg
+    
